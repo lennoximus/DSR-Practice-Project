2911074c8458dc534f99dc93f280b532e6477cf3
lennoximus <lennoximus@gmail.com>
2016-05-01 13:16:12 +0300

    Первая версия
    Сделал небольшую заготовку под переход на Swing. Добавил своего рода
    логгер, который будет предоставлять всю необходимую помощь и информацию
    для пользования приложением.
    Доступ к файлу настроек будет осуществляться через окно диалога открытия
    файла, к окну открытия применяется фильтр по расширению .xls .
    Сделал некоторые наработки в виде Entity-классов для работы с БД
    (тестовая схема, на которой планировалось проверять работоспособность, -
    на картинке в корневой директории) через ORM-фреймворки. Добиться успеха
    через ORMLite не получилось, т.к. ошибка с определением типа БД, в
    дальнейшем, попробую через MyBatis.

--- src/main/java/com/mycompany/autointegrationapp/FileChooserDialog.java
+++ src/main/java/com/mycompany/autointegrationapp/FileChooserDialog.java
@@ -1,30 +1,37 @@
-
 // Данный класс и FileFilter – возможный вариант открытия файла настроек xls.
-
 package com.mycompany.autointegrationapp;
 
 import javax.swing.*;
 import java.io.*;
 
 public class FileChooserDialog extends JFrame {
-    String fName = null;
-    String line = null;
-    
-    
+
+    public String fileName = null;
+
+    String getFileAbsolutePath() {
+        return (this.fileName);
+    }
+
+    public void setFileAbsolutePath(String path) {
+        this.fileName = path;
+    }
+
     FileChooserDialog() {
         setBounds(0, 0, 500, 500);
         JFileChooser dialog = new JFileChooser();
         dialog.setFileFilter(new FileFilter(".xls", ""));
         dialog.showOpenDialog(this);
         File file = dialog.getSelectedFile();
-        fName = file.getAbsolutePath();
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(fName));
-            /*while ((line = in.readLine()) != null) {
-                textArea.append(line);
-                textArea.append("\n");
-                }*/
-                in.close();
-        } catch (IOException ex) {System.out.println(ex);}
+        setFileAbsolutePath(file.getAbsolutePath());
+        /*try {
+         BufferedReader in = new BufferedReader(new FileReader(fName));
+         /*while ((line = in.readLine()) != null) {
+         textArea.append(line);
+         textArea.append("\n");
+         }
+         in.close();
+         } catch (IOException ex) {
+         System.out.println(ex);
+         }*/
     }
 }
